8c38ee4852b53224e531c75fb3e25b20f5c200a5
Weiwei Zhu <zhuwwpku@gmail.com>
2018-04-16 17:52:10 +0800

    moving some gragh definition actions into the init of the Resnet class.

--- ResNet13_wanghf/predict_by_pb.py
+++ ResNet13_wanghf/predict_by_pb.py
@@ -14,21 +14,20 @@ import numpy as np
 import tensorflow as tf
 import cv2
 
-class ResNet_predict():
-    def __init__(self, path):
+
+class ResNet_predict(object):
+    def __init__(self):
         self.num_classes = 2
         self.image_size = 64
         self.mean = np.array([127.5])
         self.pb_path = "model/frozen_model.pb"
-        self.imgs = self.get_data(path)
+        with open(self.pb_path, "rb") as f:
+            self.graph_def_str = f.read()
 
         with tf.Graph().as_default():
-            output_graph_def = tf.GraphDef()
+            self.graph_def = tf.GraphDef()
+            self.graph_def.ParseFromString(self.graph_def_str)
 
-            with open(self.pb_path, "rb") as f:
-                output_graph_def.ParseFromString(f.read())
-                tf.import_graph_def(output_graph_def, name="")
-                self.predict()
 
     def get_data(self, path):
         img = cv2.imread(path).astype('float64')
@@ -38,21 +37,30 @@ class ResNet_predict():
         imgs0[0, :, :, 0] = img[0, :, :, 0]
         return imgs0
 
-    def predict(self):
+    def predict(self, path):
+
+        imgs = self.get_data(path)
+
+        tf.import_graph_def(self.graph_def, name="")
 
         with tf.Session() as sess:
                 # init = tf.global_variable_initializer()
                 # sess.run(init)
             input_x = sess.graph.get_tensor_by_name("input:0")
             output_label = sess.graph.get_tensor_by_name("output:0")
-            result = sess.run(output_label, feed_dict={input_x: self.imgs})
+            result = sess.run(output_label, feed_dict={input_x: imgs})
             label = np.argmax(result, 1)
             self.result = label
 
+        return self.result
+
 if __name__ == '__main__':
+    from threadit import threadit
     path = "dataset/1.bmp"
-    resnet_pre = ResNet_predict(path)
-    result = resnet_pre.result
+    resnet_pre = ResNet_predict()
+    #result = resnet_pre.predict(path)
+    inputarray = [[path] for i in range(10)]
+    result = threadit(resnet_pre.predict, inputarray)
     print(result)
 
 
